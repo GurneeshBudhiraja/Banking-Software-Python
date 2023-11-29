c623ca9a87f5e5c79a94379ac2c6e6dba9e07b08
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2023-11-28 19:38:51 -0500

    Till cx done

---
+++ cx.py
@@ -0,0 +1,11 @@
+class Customer:
+  def verified(name,postal_code):
+    return True
+  def accountsummary(name,postal_code):
+    ...
+  def currency_rate(name,postal_code):
+    ...
+  def valid_postal(new_postal):
+    return 0
+  def update_postal(name,postal_code,new_postal):
+    ...
